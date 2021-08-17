fee9bf9730078345179b1e6760a5933061f55831
Ayrton Domingos Lorenceti <ayrton.lorenceti@compasso.com.br>
2021-08-17 15:43:51 -0300

    Atualizada as versões dos pacotes e configurada as variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CityController } from "./controllers/city.controller";
 import { ClientController } from "./controllers/client.controller";
 
@@ -9,7 +11,17 @@ import { ClientService } from "./services/client.service";
 
 @Module({
   imports: [
-    // ConfigModule.forRoot()
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'test',
+    //   entities: [],
+    //   synchronize: true,
+    // })
+    ConfigModule.forRoot()
   ],
   controllers: [
     CityController, 
