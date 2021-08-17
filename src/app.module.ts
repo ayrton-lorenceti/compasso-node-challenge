6a35f26b7cab8e6ef4996622d7c87d0a84be570b
Ayrton Domingos Lorenceti <ayrton.lorenceti@compasso.com.br>
2021-08-17 08:54:55 -0300

    Adição dos novos controllers ao AppModules e exclusão do controller não utilizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
+import { CityController } from "./controllers/city.controller";
+import { ClientController } from "./controllers/client.controller";
+
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [CityController, ClientController],
   providers: [AppService],
 })
 export class AppModule {}
