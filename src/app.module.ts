a1c3d777e1c51000ad284d717eed0d22cb279548
Ayrton Domingos Lorenceti <ayrton.lorenceti@compasso.com.br>
2021-08-17 09:03:33 -0300

    Criação do ClientService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { CityController } from "./controllers/city.controller";
 import { ClientController } from "./controllers/client.controller";
 
 import { CityService } from './services/city.service';
+import { ClientService } from "./services/client.service";
 
 @Module({
   imports: [],
-  controllers: [CityController, ClientController],
-  providers: [CityService],
+  controllers: [
+    CityController, 
+    ClientController
+  ],
+  providers: [
+    CityService,
+    ClientService
+  ],
 })
 export class AppModule {}
---
+++ src/services/client.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ClientService {
+  create() {
+    return "";
+  }
+
+  getById() {
+    return "";
+  }
+
+  getByName() {
+    return "";
+  }
+
+  updateName() {
+    return "";
+  }
+
+  delete() {
+    return "";
+  }
+}
